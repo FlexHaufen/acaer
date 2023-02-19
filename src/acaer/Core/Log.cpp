34bb5c683cbc8b11a0b3a541a22b7a0b3772d5d7
FlexHaufen <ibims1k0tt@gmail.com>
2023-02-19 16:10:44 +0100

    Fixed Typo

--- src/acaer/Core/Log.cpp
+++ src/acaer/Core/Log.cpp
@@ -28,7 +28,7 @@ namespace Acaer {
 	void Log::Init() {
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Lychee.log", true));
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Acaer.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
