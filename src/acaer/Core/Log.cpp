fb6d5ca3ce0f57064035832a4875d5be8bceeac0
FlexHaufen <ibims1k0tt@gmail.com>
2023-03-30 19:06:19 +0200

    Logger update

--- src/acaer/Core/Log.cpp
+++ src/acaer/Core/Log.cpp
@@ -30,18 +30,21 @@ namespace Acaer {
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Acaer.log", true));
 
-		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
-
-		s_CoreLogger = std::make_shared<spdlog::logger>("ACAER [CORE]", begin(logSinks), end(logSinks));
+		// ** Console Log **
+		//							"[DD.MM.YY HH:MM:SS.ms][pid][tid] [CORE]: msg]"
+		logSinks[0]->set_pattern("%^[%d.%m.%C %T.%e][%P][%t] %n: %v%$");
+		s_CoreLogger = std::make_shared<spdlog::logger>("[CORE]", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
 		s_CoreLogger->flush_on(spdlog::level::trace);
 
-		s_ClientLogger = std::make_shared<spdlog::logger>("ACAER  [APP]", begin(logSinks), end(logSinks));
+		// ** File Log **
+		//							"[DD.MM.YY HH:MM:SS.ms][pid][tid][level]  [APP]: msg]"
+		logSinks[1]->set_pattern("%^[%d.%m.%C %T.%e][%P][%t][%l]\t %n: %v");
+		s_ClientLogger = std::make_shared<spdlog::logger>("[APP]", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_ClientLogger);
-		s_ClientLogger->set_level(spdlog::level::trace);
-		s_ClientLogger->flush_on(spdlog::level::trace);
+		s_ClientLogger->set_level(spdlog::level::info);
+		s_ClientLogger->flush_on(spdlog::level::info);
 
 		AC_CORE_INFO("Logger Initialized");
 	}
